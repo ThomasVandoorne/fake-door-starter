b762e8d30fbff50823cae89a07c608b796970223
Your Name Here <your_email@student.howest.be>
2022-10-25 13:41:34 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
+import JSConfetti from "../node_modules/js-confetti/dist/es/index.js";
+import "../css/style.css";
 
 const init = () => {
   const $form = document.querySelector("form");
+  const jsConfetti = new JSConfetti();
+
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
-  });
-}
 
+    jsConfetti.addConfetti();
+  });
+};
 
 init();
