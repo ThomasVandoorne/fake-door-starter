b66b0f97ca78f402a5be96a8a27157da369a85c6
Your Name Here <your_email@student.howest.be>
2022-10-25 13:51:46 +0200

    tweaks

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-import JSConfetti from "../node_modules/js-confetti/dist/es/index.js";
 import "../css/style.css";
+import JSConfetti from "../node_modules/js-confetti/dist/es/index.js";
 
 const init = () => {
   const $form = document.querySelector("form");
